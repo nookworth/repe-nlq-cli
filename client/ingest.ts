da94bfa97a22069f237f5d2b4af9edc3e5df2fa3
nookworth <nooksack@gmail.com>
2025-05-22 22:59:07 -0600

    insert jsonl into db

--- client/ingest.ts
+++ client/ingest.ts
@@ -1,13 +1,23 @@
 import { open, writeFile } from "node:fs/promises";
-import { parse } from "@fast-csv/parse";
+import { parse, Row } from "@fast-csv/parse";
 import path from "node:path";
 
 export const ingest = async (filePath: string) => {
-  const file = await open(filePath)
+  const file = await open(path.join('./data', filePath))
   const fileName = path.basename(filePath, path.extname(filePath))
   const arr: object[] = []
 
   const parsed = file.createReadStream().pipe(parse({ headers: true, ignoreEmpty: true }))
+      .transform((data: Row) => {
+        const transformed = {}
+
+        for (const key in data) {
+          if (key === '') continue
+          transformed[key.toLowerCase()] = data[key]
+        }
+
+        return transformed
+      })
       .on('error', error => console.error(error))
       .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows from ${filePath}`))
 
@@ -15,16 +25,10 @@ export const ingest = async (filePath: string) => {
     arr.push(row)
   }
 
-  arr.forEach(row => {
-    if (row.hasOwnProperty('')) {
-      delete row[''];
-    }
-  });
-  
-  const processed = JSON.stringify(arr).replace(/\},/g, '},\n')
+  const processed = JSON.stringify(arr).replace(/\},/g, '}\n').replace(/[\[\]]/g, '')
 
-  await writeFile(path.join(path.dirname(filePath), `${fileName}.jsonl`), processed)
-  console.log(`Processed ${arr.length} rows and saved to ${path.join(path.dirname(filePath), `${fileName}.jsonl`)}`)
+  await writeFile(path.join('./data', `${fileName}.jsonl`), processed)
+  console.log(`Processed ${arr.length} rows and saved to ${path.join('./data', `${fileName}.jsonl`)}`)
 
   return processed
 };
--- client/prompt.ts
+++ client/prompt.ts
@@ -1,8 +1,49 @@
-import { readdir } from "node:fs/promises";
-import select from "@inquirer/select";
 import { ingest } from "./ingest";
+import { open, readdir, rm } from "node:fs/promises";
+import path from "node:path";
+import sqlite from 'sqlite3';
+import select from "@inquirer/select";
 
-const main = async () => {
+/**@todo loading indicator for initializing db */
+const dbPath = './server/migrations/rent_roll.db'
+const db = new sqlite.Database(dbPath);
+const sql = `
+CREATE TABLE IF NOT EXISTS rent_roll (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unit TEXT NOT NULL,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    sq_ft INTEGER,
+    autobill DECIMAL(10,2) NOT NULL,
+    deposit DECIMAL(10,2) NOT NULL,
+    moved_in DATE,
+    lease_ends DATE,
+    status TEXT NOT NULL
+)`
+
+db.exec(sql, error => {
+    if (error) console.error(error)
+})
+
+const insertPrompt = async () => {
+    const files = await readdir("./data");
+    const fileChoice = await select({
+        message: "Select a file to insert:",
+        choices: files.filter((file) => file.endsWith(".jsonl")).map((file) => ({ name: file, value: file })),
+    });
+    const file = await open(path.join("./data", fileChoice));
+
+    for await (const line of file.readLines()) {
+        const json = JSON.parse(line)
+        db.run("INSERT INTO rent_roll (unit, name, type, sq_ft, autobill, deposit, moved_in, lease_ends, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [json.unit, json.name, json.type, json.sq_ft, json.autobill, json.deposit, json.moved_in, json.lease_ends, json.status], (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+}
+
+const ingestPrompt = async () => {
     const files = await readdir("./data");
     const file = await select({
         message: "Select a file to ingest:",
@@ -11,4 +52,4 @@ const main = async () => {
     await ingest(file);
 };
 
-main()
+ingestPrompt().then(() => insertPrompt())
