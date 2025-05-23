987bebce070ebaa7b5f40a6ad7bd7fd5e70cb408
nookworth <nooksack@gmail.com>
2025-05-22 20:47:05 -0600

    better filename handling

--- client/ingest.ts
+++ client/ingest.ts
@@ -1,8 +1,10 @@
-import { open } from "node:fs/promises";
+import { open, writeFile } from "node:fs/promises";
 import { parse } from "@fast-csv/parse";
+import path from "node:path";
 
 export const ingest = async (filePath: string) => {
   const file = await open(filePath)
+  const fileName = path.basename(filePath, path.extname(filePath))
   const arr: object[] = []
   const parsed = file.createReadStream().pipe(parse({ headers: true, ignoreEmpty: true }))
       .on('error', error => console.error(error))
@@ -13,5 +15,7 @@ export const ingest = async (filePath: string) => {
   }
 
   const processed = JSON.stringify(arr).replace(/\},/g, '},\n')
+  await writeFile(path.join(path.dirname(filePath), `${fileName}.jsonl`), processed)
+  console.log(`Processed ${arr.length} rows and saved to ${path.join(path.dirname(filePath), `${fileName}.jsonl`)}`)
   return processed
 };
